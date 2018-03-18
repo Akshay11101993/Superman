2d63cecdb41cf3ad032a38a3faf26fa562d2497d
Akshay11101993 <37494921+Akshay11101993@users.noreply.github.com>
2018-03-18 12:53:44 +0530

    Update LoginController.java

--- ebay-web/src/main/java/LoginController.java
+++ ebay-web/src/main/java/LoginController.java
@@ -3,6 +3,8 @@ public class LoginController {
 
 	private void syso() {
 		System.out.println("This is my first contoller");
+		System.out.println("This is my first contoller +++++++++++++");
+		System.out.println("This is my first contoller#############");
 
 	}
 }
