52fd9401ad5cc5778a271f6a5cf8cca807c1d811
s <s@s-PC>
2018-03-19 12:51:14 +0530

    new

--- ebay-web/src/main/java/LoginController.java
+++ ebay-web/src/main/java/LoginController.java
@@ -6,8 +6,6 @@ public class LoginController {
 		System.out.println("This is my first contoller +++++++++++++");
 		System.out.println("This is my first contoller#############");
 		System.out.println("This is my first contollerDemo for fetch");
-		System.out.println("This is my Second");
-		System.out.println("This is my third");
-		System.out.println("This is my fourth");
+		
 	}
 }
