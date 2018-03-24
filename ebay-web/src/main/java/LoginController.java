50bf4ed551e4942487a68f0c9acb06073dce266b
vikas <vikas.patidar110@gmail.com>
2018-03-24 11:27:13 +0530

    files

--- ebay-web/src/main/java/LoginController.java
+++ ebay-web/src/main/java/LoginController.java
@@ -6,6 +6,9 @@ public class LoginController {
 		System.out.println("This is my first contoller +++++++++++++");
 		System.out.println("This is my first contoller#############");
 		System.out.println("This is my first contollerDemo for fetch");
+		
+		System.out.println("This is my first stash demo 111111111111111");
+		System.out.println("This is my first stash demo 55555555555555");
 
 	}
 }
---
+++ ebay-web/src/main/java/SMSController.java
@@ -0,0 +1,8 @@
+
+public class SMSController {
+	private void syso() {
+
+		System.out.println("This is my first SMS controller for fetch");
+
+	}
+}
